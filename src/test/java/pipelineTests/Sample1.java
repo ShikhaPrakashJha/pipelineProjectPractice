ea3f02728888fe67ecedb0b67e062fd9a094aaa4
ShikhaPrakashJha <shikha.prakash1994@gmail.com>
2023-03-13 13:58:54 +0530

    committed

---
+++ src/test/java/pipelineTests/Sample1.java
@@ -0,0 +1,21 @@
+package pipelineTests;
+
+import org.testng.annotations.Test;
+
+public class Sample1 {
+	@Test
+	public void test11() {
+		System.out.println("test---11");
+	}
+	@Test
+	public void test12() {
+		System.out.println("test---12");
+	}
+	
+	@Test
+	public void test13() {
+		System.out.println("test---13");
+	}
+	
+}
+
---
+++ src/test/java/pipelineTests/Sample2.java
@@ -0,0 +1,21 @@
+package pipelineTests;
+
+import org.testng.annotations.Test;
+
+public class Sample2 {
+	@Test
+	public void test14() {
+		System.out.println("test---14");
+	}
+	@Test
+	public void test15() {
+		System.out.println("test---15");
+	}
+	
+	@Test
+	public void test16() {
+		System.out.println("test---16");
+	}
+	
+}
+
